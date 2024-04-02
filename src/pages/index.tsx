1f56df5a285bda1d0b25d0fc4f4c0961d2546109
vedunuri sujith <vedunurisujith01@gmail.com>
2024-04-02 02:54:45 -0500

    updated changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { VideoGrid } from "./components/VideoGrid";
-import { AppBar } from "./components/AppBar";
+import { VideoGrid } from "./VideoGrid";
+import { AppBar } from "./AppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
