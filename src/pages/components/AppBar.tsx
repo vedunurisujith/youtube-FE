0155dd8fa834467efcca62268b89a8f95a9ebf9e
vedunuri sujith <vedunurisujith01@gmail.com>
2024-04-02 01:31:02 -0500

    added appbar

--- src/pages/components/AppBar.tsx
+++ src/pages/components/AppBar.tsx
@@ -1,24 +1,31 @@
 export const AppBar = () => {
- return     <div className="grid grid-cols-3">
-                    <div className="col-start-1"> 
-                    <img className="h-12 cursor-pointer" src="/logo.png" alt="Youtube"/>
-                    </div>
-                    <div className="cols-center-1 ">
-                        Search Bar
-                    </div>
-                    <div className="col-start-1 col-end-1">
-                        notifications
-                        </div>
+    return    <div className="grid grid-cols-3">
+                    <div className="col-start-1 pl-3 pt-3"> 
+                        <img className="h-11 ml-2 cursor-pointer" src="/logo.png" alt="Youtube"/>
                     </div>
 
-{/* <div className="grid grid-cols-12 bg-red-100 gap-auto "> */}
-//     <div className=" col-start-3 col-span-4 bg-blue-100">
-//         logo
-//     </div>
-//     <div className="col-center-5 bg-green-100">
-//             searchabar
-//     </div>
-//     <div className="col-end-4 bg-slate-100">
-//         otherIcons
-//     </div>
+                    <div>
+    <div className="relative w-full mt-4">
+        <input type="text" id="simple-search" className="bg-gray-50 h-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-blue-500" placeholder="Search" required />
+        <div className="absolute inset-y-0 end-4 flex items-center ps-3 pointer-events-none">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+        </div>
+    </div>
+                    </div>
+                    <div className="grid grid-cols-10">
+                        <div className="col-span-8">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mt-5 ml-6 cursor-pointer">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+                        </svg>
+                        </div>
+                    <div className="col-span-2 mt-5 mr-2 flex justify-center ">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                    </div>
+
+                    </div>
+                </div>
 }
